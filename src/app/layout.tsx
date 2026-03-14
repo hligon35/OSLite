e45e699c85311493fd8eead85c5a628ff2caf87f
Harold Ligon <157246297+hligon35@users.noreply.github.com>
2026-03-14 00:18:16 -0400

    added futura font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -92,6 +92,13 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <meta httpEquiv="x-ua-compatible" content="IE=edge" />
+        <meta name="format-detection" content="telephone=no" />
+        <link rel="preconnect" href="https://use.typekit.net" />
+        <link rel="preconnect" href="https://p.typekit.net" crossOrigin="anonymous" />
+        <link rel="stylesheet" href="https://use.typekit.net/elz2zdv.css" />
+      </head>
       <body>
         <DebugGlobalHandlers />
         <DebugConsolePanel />
